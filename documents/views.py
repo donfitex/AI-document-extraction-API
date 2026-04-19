398d2a4050c400923378b37d9f6c0ad550e027b1
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-04-19 18:46:16 +0100

    fix(dev): run celery tasks synchronously without redis
    - Enabled CELERY_TASK_ALWAYS_EAGER
    - Replaced delay() with direct task call
    - Removed dependency on Redis for development
    
    Simplifies local testing environment

--- documents/views.py
+++ documents/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+
+from documents.tasks import process_document
 from .models import Document
 from .serializers import DocumentSerializer
 
@@ -10,5 +12,12 @@ class DocumentViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return Document.objects.filter(user=self.request.user)
 
+    # def perform_create(self, serializer):
+    #     serializer.save(user=self.request.user)
+
+    #Replace delay() with direct task call
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        doc = serializer.save(user=self.request.user)
+
+        from .tasks import process_document
+        process_document(doc.id) 
