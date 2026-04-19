83673c6503e6b3274897597a4cb933ac1aa5fb2a
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-04-19 19:17:15 +0100

    feat(media): enable media file serving in development
    - Configured static serving for uploaded files
    - Linked MEDIA_URL to MEDIA_ROOT
    - Enabled browser access to uploaded documents

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,9 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 router = DefaultRouter()
 router.register(r'documents', DocumentViewSet, basename='document')
 
@@ -34,3 +37,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
