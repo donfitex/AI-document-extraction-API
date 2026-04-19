3f635bf15d19ebfd577a89db82fd048bcaf238e9
Raphael_Eze <raphaelifeanyieze@gmail.com>
2026-04-19 19:34:54 +0100

    feat(admin): register Document model in admin panel
    - Added DocumentAdmin configuration
    - Enabled filtering and search
    - Made extracted_data read-only
    
    Improves backend visibility and debugging

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Document
 
 # Register your models here.
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "status", "created_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("user__username",)
+    readonly_fields = ("status", "extracted_data", "created_at")
